5793ec0f696df4907340af38b9db2285fd0f2ff4
cassandra.rommel@gmail.com <cassandra.rommel@gmail.com@267125f9-9a0c-649b-77ca-9e6f8226adf6>
2012-05-07 06:06:17 +0000

    Chicken jumps constantly and we can move him with the left and right arrow keys appropriately! We can adjust some numbers to make it harder or easier to reach platforms.

--- Doodle-Duel2/Doodle-Duel2/Doodle-Duel2/BasicModel.cs
+++ Doodle-Duel2/Doodle-Duel2/Doodle-Duel2/BasicModel.cs
@@ -17,14 +17,40 @@ namespace Doodle_Duel2
 
         public Model model { get; protected set; }
         protected Matrix world = Matrix.Identity;
+        private float modelRotation;
+        private Vector3 modelPosition;
+        private float initialHeight;
+        private float jumpTime = 0; 
+        private float velocity = 15f;
+        private float gravity = 4.5f; 
 
-        public BasicModel(Model m)
+        public BasicModel(Model m, float rotation, Vector3 position)
         {
             model = m;
+            modelRotation = rotation;
+            modelPosition = position;
+            initialHeight = position.Y;
         }
 
         public virtual void Update()
         {
+            if (jumpTime > velocity/gravity)
+            {
+                jumpTime = 0;
+            }
+            else
+            {
+                float timeSquared = (float)Math.Pow(jumpTime, 2);
+                float initialVelocity = velocity * jumpTime;
+                float gravityLoss = gravity * timeSquared;
+                modelPosition = new Vector3(modelPosition.X, initialVelocity - gravityLoss + initialHeight, modelPosition.Z);
+                jumpTime += .05f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                modelPosition += new Vector3(-.5f, 0, 0);
+            else if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                modelPosition += new Vector3(.5f, 0, 0);
+
         }
 
         public virtual Matrix getWorld()
@@ -45,6 +71,7 @@ namespace Doodle_Duel2
                     be.Projection = camera.projection;
                     be.View = camera.view;
                     be.World = getWorld() * mesh.ParentBone.Transform;
+                    be.World = transforms[mesh.ParentBone.Index] * Matrix.CreateRotationY(modelRotation) * Matrix.CreateTranslation(modelPosition); ;
                 }
 
                 mesh.Draw();
