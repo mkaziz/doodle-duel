6e78095eb5fbf5b62cfef76abd9293c94294b065
dhruvkoul.2013@gmail.com <dhruvkoul.2013@gmail.com@267125f9-9a0c-649b-77ca-9e6f8226adf6>
2012-05-07 03:34:17 +0000

    background seems a little less jerky on my computer. hopefully it works on your guys'?

--- trunk/Doodle-Duel2/Doodle-Duel2/Doodle-Duel2/Game1.cs
+++ trunk/Doodle-Duel2/Doodle-Duel2/Doodle-Duel2/Game1.cs
@@ -11,9 +11,6 @@ using Microsoft.Xna.Framework.Media;
 
 namespace Doodle_Duel2
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
@@ -28,12 +25,6 @@ namespace Doodle_Duel2
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
@@ -51,10 +42,6 @@ namespace Doodle_Duel2
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
@@ -63,20 +50,11 @@ namespace Doodle_Duel2
             // TODO: use this.Content to load your game content here
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
@@ -88,10 +66,6 @@ namespace Doodle_Duel2
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Green);
