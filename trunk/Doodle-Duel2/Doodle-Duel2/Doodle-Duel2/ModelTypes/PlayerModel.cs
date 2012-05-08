99e99e9a3b0609ac4d21668ad25dfd32e75653f7
cassandra.rommel@gmail.com <cassandra.rommel@gmail.com@267125f9-9a0c-649b-77ca-9e6f8226adf6>
2012-05-08 02:42:50 +0000

    Added shadows!

---
+++ trunk/Doodle-Duel2/Doodle-Duel2/Doodle-Duel2/ModelTypes/PlayerModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace Doodle_Duel2
+{
+    public class PlayerModel
+    {
+
+        public Model model { get; protected set; }
+        protected Matrix world = Matrix.Identity;
+
+        //Initial vars
+        private float modelRotation;
+        private Vector3 modelPosition;
+        private float modelScale;
+        private string tag;
+
+        public Vector3 Position 
+        {
+            get { return new Vector3(modelPosition.X,initialHeight-2, modelPosition.Z); }
+        }
+
+        public float CurrentHeight 
+        {
+            get { return modelPosition.Y; }
+        }
+        //Vars for smooth jumping
+        private float initialHeight;
+        private float jumpTime = 0;
+        private float velocity = 20f;//Change around to make jumping higher/lower
+        private float gravity = 4.5f; //Can change around to make jumping faster/slower
+
+        public PlayerModel(Model m, float rotation, Vector3 position, float scale, string t)
+        {
+            model = m;
+            tag = t; 
+            modelRotation = rotation;
+            modelPosition = position;
+            initialHeight = position.Y;
+            modelScale = scale;
+        }
+
+        public virtual void Update()
+        {
+            if (jumpTime > velocity / gravity)
+            {
+                jumpTime = 0; //restart jump cycle. Right now it prevents user from falling out of frame
+            }
+            else
+            {
+                //utilizes y = .5at^s + Vot + yo
+                //that is acceleration downward due to gravity, initial velocity, and the initial position
+                float timeSquared = (float)Math.Pow(jumpTime, 2);
+                float initialVelocity = velocity * jumpTime;
+                float gravityLoss = gravity * timeSquared;
+                modelPosition = new Vector3(modelPosition.X, initialVelocity - gravityLoss + initialHeight, modelPosition.Z);
+                jumpTime += .05f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                modelPosition += new Vector3(-.5f, 0, 0);
+            else if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                modelPosition += new Vector3(.5f, 0, 0);
+            else if (Keyboard.GetState().IsKeyDown(Keys.Up))
+                modelPosition += new Vector3(0, 0, .5f);
+            else if (Keyboard.GetState().IsKeyDown(Keys.Down))
+                modelPosition += new Vector3(0, 0, -.5f);
+
+        }
+
+        public virtual Matrix getWorld()
+        {
+            return world;
+        }
+
+        public void Draw(Camera camera)
+        {
+            Matrix[] transforms = new Matrix[model.Bones.Count];
+            model.CopyAbsoluteBoneTransformsTo(transforms);
+
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect be in mesh.Effects)
+                {
+                    be.EnableDefaultLighting();
+                    be.Projection = camera.projection;
+                    be.View = camera.view;
+                    be.World = getWorld() * mesh.ParentBone.Transform;
+                    be.World = transforms[mesh.ParentBone.Index] * Matrix.CreateRotationY(modelRotation) * Matrix.CreateScale(modelScale) * Matrix.CreateTranslation(modelPosition);
+                }
+
+                mesh.Draw();
+            }
+
+
+        }
+
+
+    }
+}
