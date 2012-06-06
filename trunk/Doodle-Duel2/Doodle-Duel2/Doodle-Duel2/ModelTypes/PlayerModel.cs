938b69204ec865dcec84a3dc8ce3fb703b00af4d
cassandra.rommel@gmail.com <cassandra.rommel@gmail.com@267125f9-9a0c-649b-77ca-9e6f8226adf6>
2012-06-06 00:29:14 +0000

    Space bar shoots projectiles that destroy enemies! woo!

--- trunk/Doodle-Duel2/Doodle-Duel2/Doodle-Duel2/ModelTypes/PlayerModel.cs
+++ trunk/Doodle-Duel2/Doodle-Duel2/Doodle-Duel2/ModelTypes/PlayerModel.cs
@@ -9,7 +9,6 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
-
 namespace Doodle_Duel2
 {
     public class PlayerModel : BasicModel
@@ -58,6 +57,11 @@ namespace Doodle_Duel2
         private float gravity = 4.5f; //Can change around to make jumping faster/slower
         private float currentVelocity;
 
+        //Vars for projectiles
+        private ModelManager manager;
+        //To prevent multiple projectiles in one key press
+        private KeyboardState oldkeyboard; 
+
         public float initialVelocity 
         {
             get { return velocity; }
@@ -71,9 +75,11 @@ namespace Doodle_Duel2
             modelPosition = position;
             initialHeight = position.Y;
             modelScale = scale;
+            oldkeyboard = Keyboard.GetState(); 
         }
 
-        public PlayerModel(Model m, float rotation, Vector3 position, float scale, string t)   : base(m, rotation, position, scale)
+        public PlayerModel(Model m, float rotation, Vector3 position, float scale, string t, ModelManager man)   
+            : base(m, rotation, position, scale)
         {
             model = m;
             tag = t;
@@ -81,6 +87,7 @@ namespace Doodle_Duel2
             modelPosition = position;
             initialHeight = position.Y;
             modelScale = scale;
+            manager = man;
         }
 
         public virtual void Update()
@@ -95,14 +102,51 @@ namespace Doodle_Duel2
                 modelPosition = new Vector3(modelPosition.X, initialVelocity - gravityLoss + initialHeight, modelPosition.Z);
                 jumpTime += .05f;
 
-            if (Keyboard.GetState().IsKeyDown(Keys.Right))
-                modelPosition += new Vector3(-.5f, 0, 0);
-            else if (Keyboard.GetState().IsKeyDown(Keys.Left))
-                modelPosition += new Vector3(.5f, 0, 0);
-            else if (Keyboard.GetState().IsKeyDown(Keys.Up))
-                modelPosition += new Vector3(0, 0, .5f);
-            else if (Keyboard.GetState().IsKeyDown(Keys.Down))
-                modelPosition += new Vector3(0, 0, -.5f);
+                if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                    modelPosition += new Vector3(-.5f, 0, 0);
+                else if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                    modelPosition += new Vector3(.5f, 0, 0);
+                else if (Keyboard.GetState().IsKeyDown(Keys.Up))
+                    modelPosition += new Vector3(0, 0, .5f);
+                else if (Keyboard.GetState().IsKeyDown(Keys.Down))
+                    modelPosition += new Vector3(0, 0, -.5f);
+                else if (Keyboard.GetState().IsKeyDown(Keys.Space) && !oldkeyboard.IsKeyDown(Keys.Space))
+                    Fire();
+
+                oldkeyboard = Keyboard.GetState();
+                
+            List<BasicModel> needtoremove = new List<BasicModel>();
+               
+            if (manager.models.Count > 0)
+                {
+                    foreach (BasicModel model in manager.models)
+                    {
+                        //First check if it is off screen
+                        if (model.modelPosition.X > manager.Game.GraphicsDevice.Viewport.Width || model.modelPosition.Y > manager.Game.GraphicsDevice.Viewport.Height)
+                        {
+                            needtoremove.Add(model);
+                        }
+                        else if (manager.enemyModels.Count > 0)
+                        {
+                            if ((manager.enemyModels[0].modelPosition - model.modelPosition).X < 2 && (manager.enemyModels[0].modelPosition - model.modelPosition).Y < 2)
+                            {
+                                needtoremove.Add(model);
+                                //manager.models.Remove(model);
+                                manager.enemyModels.Remove(manager.enemyModels[0]);
+                            }
+                            else
+                                model.modelPosition += (manager.enemyModels[0].modelPosition - model.modelPosition) * .1f;
+                        }
+                        else
+                            model.modelPosition += new Vector3(0, .7f, 0);
+                    }
+                }
+            if(needtoremove.Count > 0)
+                for (int i = 0; i < needtoremove.Count; i++)
+                {
+                    manager.models.Remove(needtoremove[i]);
+                    needtoremove.Remove(needtoremove[i]);
+                }
 
         }
 
@@ -131,6 +175,11 @@ namespace Doodle_Duel2
             }
 
 
+        }
+
+        public void Fire()
+        {
+            manager.models.Add(new BasicModel(manager.Game.Content.Load<Model>(@"projectile"),0, modelPosition, .1f));
         }
 
     }
