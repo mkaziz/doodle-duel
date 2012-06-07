b89a7823f6dff09f8a83f1ce231cd3bc837943d4
dhruvkoul.2013@gmail.com <dhruvkoul.2013@gmail.com@267125f9-9a0c-649b-77ca-9e6f8226adf6>
2012-06-07 21:18:05 +0000

    height fixed :D

--- Doodle-Duel2/Doodle-Duel2/Doodle-Duel2/Game1.cs
+++ Doodle-Duel2/Doodle-Duel2/Doodle-Duel2/Game1.cs
@@ -24,7 +24,7 @@ namespace Doodle_Duel2
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
 
-            graphics.PreferredBackBufferWidth = 900;
+            graphics.PreferredBackBufferWidth = 780;
             graphics.PreferredBackBufferHeight = 700;
 
             // Create the screen manager component.
